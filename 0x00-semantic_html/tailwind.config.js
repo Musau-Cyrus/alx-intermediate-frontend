62abed9ac22b4cca69b779b50c3def10644374f1
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-05-18 20:56:03 +0300

    Modified

--- 0x00-semantic_html/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-//**@type {import('tailwindcss').config}*/
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "../0x00-semantic_html/**/*.html"],
   theme: {
     extend: {},
   },
